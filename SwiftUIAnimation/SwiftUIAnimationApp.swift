22e16a0f60f62b7020ff764138fbe31ecc1bd2e0
poga07 <chris@168money.com.tw>
2025-01-21 10:57:06 +0800

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by 黃柏嘉 on 2025/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
